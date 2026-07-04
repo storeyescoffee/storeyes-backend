876d8c2a27bf11be24d67396acb62be45c1e7070
benaarrouch <benaarrouch.mohamed01@gmail.com>
2026-07-04 03:32:28 +0100

    fix the inactif position types bug

--- src/main/java/io/storeyes/storeyes_coffee/charges/dto/PersonnelTypeResponse.java
+++ src/main/java/io/storeyes/storeyes_coffee/charges/dto/PersonnelTypeResponse.java
@@ -1,5 +1,6 @@
 package io.storeyes.storeyes_coffee.charges.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 public class PersonnelTypeResponse {
     private Long id;
     private String name;
+    @JsonProperty("isActive")
     private boolean isActive;
     private LocalDateTime createdAt;
 }
